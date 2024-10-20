6913ac2e8b961ffdeda6acac903bafbb14ef060c
Alejandro Pineda <alejandropi301196@gmail.com>
2024-10-20 11:42:43 +0200

    Category and Products added

---
+++ eCommerceApp.Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceApp.Domain.Entities
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string? Image { get; set; }
+
+        public int Quatity { get; set; }
+        public Category? Category { get; set; }
+        
+    }
+}
